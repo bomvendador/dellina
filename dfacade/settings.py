583dae08d1791a2091efbe68dceead1a05466640
Alex <bomvendador@yandex.ru>
2018-11-04 02:05:58 +0300

    1 commit

--- dfacade/settings.py
+++ dfacade/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'dfacade.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'dellina',
-#         'USER': 'debian-sys-maint',
-#         'PASSWORD': 'alexrodin',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dellina',
+        'USER': 'debian-sys-maint',
+        'PASSWORD': 'alexrodin',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
