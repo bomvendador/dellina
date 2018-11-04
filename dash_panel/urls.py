b4ecc9cf157c6e45a87dc5d0db84edb6dfad9e60
Alex <bomvendador@yandex.ru>
2018-11-04 17:10:12 +0300

    1 commit

--- dash_panel/urls.py
+++ dash_panel/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
 ]
 
 # if settings.DEBUG:
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
