61e756860355308661cbce98cdc1e791cc8f2e70
Alex <bomvendador@yandex.ru>
2018-11-04 16:21:46 +0300

    1 commit

--- dfacade/settings.py
+++ dfacade/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '5^cci0$p(f5l^q3v@m$9%y8=3&cb*8g5d&r@$ah5)kuf1=)1+7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dellina.ru', '134.0.112.113']
+ALLOWED_HOSTS = ['dellina.ru', '134.0.112.113', 'localhost']
 
 
 # Application definition
